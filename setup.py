af3eb2a85dd4edc495ae27053fe7fb6790130c8d
Ashley Felton <ashley@ropable.com>
2021-05-07 11:13:21 +0800

    Added basic setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+version = ('0.1.2')
+
+setup(
+    name='nginx-conf-parser',
+    version=version,
+    packages=['nginx_conf_parser'],
+    description='Nginx configuration parser for Python',
+)
